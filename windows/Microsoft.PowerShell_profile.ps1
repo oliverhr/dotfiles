2b41903e6a42f2577782e92d1d7dd6607b790c35
Oliver H. Rangel <oliverhr@users.noreply.github.com>
2025-03-29 18:30:13 -0600

    Update PWSH Profile

--- windows/Microsoft.PowerShell_profile.ps1
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -1,7 +1,13 @@
+# vim: set ft=ps1 ts=4 sts=4 sw=4 et :
+
 ###########################################################
-# Powershell profile
+# Custom Powershell profile
 ###########################################################
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\robbyrussell.omp.json" | Invoke-Expression
+# https://poshcode.gitbook.io/powershell-practice-and-style/style-guide/code-layout-and-formatting
+###########################################################
+
+$omptheme = 'robbyrussell'
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\$omptheme.omp.json" | Invoke-Expression
 
 #------------------------------------------------------------------------------
 # Control how the history is being managed
@@ -9,22 +15,25 @@ oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\robbyrussell.omp.json" | In
 $PSReadLineHistoryHandler = {
     Param([string]$line)
     $patterns = @(
-        'clear'
-        'exit'
         '^ls[\s]*'
+        '^cp[\s]'
+        '^mv\s'
+        '^mkdir\s'
         '^cd[\s]*'
         '^rm\s'
         '^touch\s'
-        '^vim\s'
-        '^git\s'
-        '^g[sa][ta]$'
-        '^gsw'
-        '^g[acfldp]$'
+        'exit'
+        '^vim' '^gvim' '^pycharm' '^code' '^npp'
+        'clear','celar', 'claer'
+        '^git\s' '^g[sa][ta]$' '^gsw' '^g[acfldp]$'
+        '^:'
+        '\.\s\$PROFILE'
+        '^[:word:].exe[\s]*'
     )
     return !($line -match ($patterns -join '|'))
 }
 $PSReadLineOptions = @{
-    HistoryNoDuplicates = $false
+    HistoryNoDuplicates = $true
     HistorySearchCursorMovesToEnd = $true
     AddToHistoryHandler = $PSReadLineHistoryHandler
 }
@@ -36,41 +45,45 @@ Set-PSReadLineOption @PSReadLineOptions
 # https://www.powershellgallery.com
 #------------------------------------------------------------------------------
 
-# ---------------------------
+#------------------------------------------------------------------------------
 # Choco completions
+#------------------------------------------------------------------------------
 # Import the Chocolatey Profile that contains the necessary code to enable
 # tab-completions to function for `choco`.
 # Be aware that if you are missing these lines from your profile, tab completion
 # for `choco` will not function.
 # See https://ch0.co/tab-completion for details.
+#------------------------------------------------------------------------------
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
     Import-Module "$ChocolateyProfile"
 }
 
 #------------------------------------------------------------------------------
-# IMPORT POWERSHELL MODULES
-#------------------------------------------------------------------------------
-# https://www.powershellgallery.com
-#------------------------------------------------------------------------------
-
-# ---------------------------
 # Git module
 # https://github.com/gluons/powershell-git-aliases
 Import-Module git-aliases -DisableNameChecking
 
-# ---------------------------
+#------------------------------------------------------------------------------
 # Docker completions
 # https://github.com/matt9ucci/DockerCompletion
 Import-Module DockerCompletion
 
-# ---------------------------
+#------------------------------------------------------------------------------
 # Fuzzyfinder fzf
+# https://github.com/junegunn/fzf
+#------------------------------------------------------------------------------
 Set-PsFzfOption `
     -PSReadlineChordProvider 'Ctrl+t' <# keyboard shortcut to start a search #>`
     -PSReadlineChordReverseHistory 'Ctrl+r' <# shortcut to search on history #>`
-    -EnableAliasFuzzySetLocation <# enable command: fd #>`
     -EnableAliasFuzzyEdit <# Enable command: fe #>
+    #-EnableAliasFuzzySetLocation <# diasbled since got installed fd from scoop #>
+
+#------------------------------------------------------------------------------
+# Zoxide
+# https://github.com/ajeetdsouza/zoxide
+#------------------------------------------------------------------------------
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
 
 #------------------------------------------------------------------------------
 # Custom Stuff
@@ -102,13 +115,76 @@ Function _vim_fzf {
 }
 Set-Alias -Name edit -Value _vim_fzf
 
-#Jetbrains IDE (eg. pycharm)
-Function _pycharm {
+# Jetbrains IDE (eg. pycharm)
+Function pycharm {
+    param ([string]$File)
+    $filename = ($File) ? $File : ($args[0]) ? $args[0] : '.'
     Start-Process `
         -FilePath 'C:\Program Files\JetBrains\PyCharm 2024.3.4\bin\pycharm64.exe' `
-        -ArgumentList "nosplash","dontReopenProjects", ($args[0])
+        -ArgumentList "nosplash", "dontReopenProjects", "'$filename'"
 }
-Set-Alias -Name pycharm -Value _pycharm
 
-# https://poshcode.gitbook.io/powershell-practice-and-style/style-guide/code-layout-and-formatting
-# vim: set ft=ps1 ts=4 sts=4 sw=4 et :
+# Notepad++ simple alias to allow pass args to npp
+Set-Alias -Name npp -Value "C:\Program Files\Notepad++\notepad++.exe"
+
+# Search with Fuzzy finder -> Vim
+Function _vim_fzf {
+    Get-ChildItem . -Recurse -Attributes !Directory | Invoke-Fzf | % { vim $_  }
+}
+Set-Alias -Name edit -Value _vim_fzf
+
+#------------------------------------------------------------------------------
+# Aliases to promote laziness XD
+#------------------------------------------------------------------------------
+Function _edit_profile {
+    vim $PROFILE
+}
+Set-Alias -Name psprofile -Value _edit_profile
+
+Function _source_profile {
+    . $PROFILE
+}
+Set-Alias -Name src -Value _source_profile
+
+#----------------------------------------------------------
+# Local stuff
+#----------------------------------------------------------
+Function _cd_projects_root {
+    $path = '~/Projects/'
+    if ($args[0]) {
+        $npath = $path + ($args[0])
+        if (Test-Path -Path $npath) {
+            $path = $npath
+        }
+    }
+    Set-Location -Path $path
+}
+Set-Alias -Value _cd_projects_root -Name projects
+
+Function _cd_project_type_by_name {
+    $sub = ($args[0]) ? '/' + $args[0] : ''
+    _cd_projects_root ($MyInvocation.InvocationName + $sub)
+}
+Set-Alias -Value _cd_project_type_by_name -Name personal
+Set-Alias -Value _cd_project_type_by_name -Name work
+
+Function _cd_work_project_by_alias {
+    # $Myinvocation.myCommand.name return the function name
+    # $Myinvocation.InvocationName return the caller/alias name
+    $projects = @{
+        'back'   = 'directory-name-for-backend'
+        'front'  = 'directory-name-for-frontend'
+        'lambda' = 'directory-name-for-microservices'
+    }
+    $path = $projects[$MyInvocation.InvocationName]
+    if ($args[0]) { $path += '/' + $args[0] }
+    # Invocation of "_cd_project_type_by_name" using alias "work"
+    work $path
+}
+Set-Alias -Value _cd_work_project_by_alias -Name api
+Set-Alias -Value _cd_work_project_by_alias -Name ui
+Set-Alias -Value _cd_work_project_by_alias -Name svc
+
+########-####-####-####-############ PowerToys CommandNotFound module
+Import-Module -Name Microsoft.WinGet.CommandNotFound
+########-####-####-####-############
