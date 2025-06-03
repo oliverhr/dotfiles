30c3ea63511868c5bb400dcc8fa244247c963c54
OliverHR <113790+oliverhr@users.noreply.github.com>
2025-06-03 11:44:49 -0600

    Add nvchad config

---
+++ nvim/nvchad/lua/configs/lspconfig.lua
@@ -0,0 +1,10 @@
+require("nvchad.configs.lspconfig").defaults()
+
+local servers = {
+  "html", "cssls",
+  "pyright", "ruff",
+  "ts_ls",
+}
+vim.lsp.enable(servers)
+
+-- read :h vim.lsp.config for changing options of lsp servers
