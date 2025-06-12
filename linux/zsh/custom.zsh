ea00a2fe48042e00915c3dc92a038063af75f352
OliverHR <113790+oliverhr@users.noreply.github.com>
2025-06-12 13:59:26 -0600

    Add ZSH configuration

---
+++ linux/zsh/custom.zsh
@@ -0,0 +1,11 @@
+# vim: set ft=sh ts=4 sw=4 :
+
+# fnm
+export PATH="$HOME/.local/share/fnm:$PATH"
+eval "`fnm env`"
+
+# UV astral
+export UV_NATIVE_TLS=true
+eval "$(uv generate-shell-completion zsh)"
+alias pip='uv pip'
+
---
+++ linux/zsh/rc.zsh
@@ -0,0 +1,59 @@
+# vim: set ft=zsh ts=4 sw=4 noet :
+# -----------------------------------------------------------------------------
+# If you come from bash you might have to change your $PATH.
+# export PATH=$HOME/bin:$HOME/.local/bin:/usr/local/bin:$PATH
+
+# -----------------------------------------------------------------------------
+# Oh-My-ZSH
+source ~/.config/zsh/ohmyzsh.zsh
+# -----------------------------------------------------------------------------
+
+# #############################################################################
+# User configuration
+# #############################################################################
+# User configuration
+
+# export MANPATH="/usr/local/man:$MANPATH"
+
+# You may need to manually set your language environment
+# export LANG=en_US.UTF-8
+
+# Preferred editor for local and remote sessions
+if [[ -n $SSH_CONNECTION ]]; then
+    export EDITOR='vim'
+else
+    export EDITOR='nvim'
+fi
+
+# -----------------------------------------------------------------------------
+# Compilation flags
+# export ARCHFLAGS="-arch $(uname -m)"
+
+# -----------------------------------------------------------------------------
+# ALIAS
+# For a full list of active aliases, run `alias`.
+test -s ~/.alias.rc && . ~/.alias.rc
+
+# -----------------------------------------------------------------------------
+# OS Specific Configuration
+# -----------------------------------------------------------------------------
+test -s ~/.config/zsh/os.conf.zsh && . ~/.config/zsh/os.conf.zsh
+#
+# -----------------------------------------------------------------------------
+# Custom configuration (Needs to be created explicitly)
+# -----------------------------------------------------------------------------
+test -s ~/.config/zsh/custom.zsh && . ~/.config/zsh/custom.zsh || true
+
+# -----------------------------------------------------------------------------
+# Tools initialization
+# -----------------------------------------------------------------------------
+
+# Direnv
+eval "$(direnv hook zsh)"
+
+# Fuzzy finder fzf
+source <(fzf --zsh)
+
+# Zoxide
+eval "$(zoxide init zsh)"
+
