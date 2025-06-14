9610ac32b77e8ada44207b5193c16ff285a4e8c1
OliverHR <oliverhr@users.noreply.github.com>
2025-06-14 17:45:33 -0600

    Add macos typora alias

--- zsh/os.macos.zsh
+++ zsh/os.macos.zsh
@@ -1,5 +1,8 @@
 # vim: set ft=zsh ts=4 sw=4 noet :
 
+# OS specific alias
+alias typora='open -a typora'
+
 # iTerm
 test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
 
