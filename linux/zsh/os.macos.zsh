2385ff035721a77ad9f8c76df731ee7b7cd52b8c
OliverHR <oliverhr@users.noreply.github.com>
2025-06-14 11:11:48 -0600

    Add macos specific config

---
+++ linux/zsh/os.macos.zsh
@@ -0,0 +1,8 @@
+# vim: set ft=zsh ts=4 sw=4 noet :
+
+# iTerm
+test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
+
+#RubyEnv
+eval "$(rbenv init - zsh)"
+
