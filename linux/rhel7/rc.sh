c2a727a32018eb6f6e938f992e64d24d7cfe4493
Oliver <113790+oliverhr@users.noreply.github.com>
2025-06-19 22:32:51 -0600

    Add XDG environment variables

--- linux/rhel7/rc.sh
+++ linux/rhel7/rc.sh
@@ -38,6 +38,10 @@ alias edit='vim $(fzf)'
 # Tools initialization
 # -----------------------------------------------------------------------------
 export XDG_RUNTIME_DIR="/tmp/run/user/$(id -u)"
+export XDG_CONFIG_HOME=$HOME/.config
+export XDG_DATA_HOME=$HOME/.local/share
+export XDG_STATE_HOME=$HOME/.local/state
+export XDG_CACHE_HOME=$HOME/.cache
 
 # Direnv
 eval "$(direnv hook bash)"
