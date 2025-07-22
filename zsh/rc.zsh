9f08efbfab2f6bb390c16f8c5e1355e091cf0ab9
Oliver <113790+oliverhr@users.noreply.github.com>
2025-07-22 06:05:20 +0000

    Update PATH zsh to add ~/bin ~/.local/bin

--- zsh/rc.zsh
+++ zsh/rc.zsh
@@ -1,7 +1,5 @@
 # vim: set ft=zsh ts=4 sw=4 noet :
 # -----------------------------------------------------------------------------
-# If you come from bash you might have to change your $PATH.
-# export PATH=$HOME/bin:$HOME/.local/bin:/usr/local/bin:$PATH
 
 # -----------------------------------------------------------------------------
 # Oh-My-ZSH
