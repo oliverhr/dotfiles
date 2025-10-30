8ebe7f1853a5a35891a069fe441ee10f21d3625c
OliverHR <113790+oliverhr@users.noreply.github.com>
2025-10-30 12:29:16 -0600

    git alias refactoring

--- windows/pwsh/includes/git-aliases.ps1
+++ windows/pwsh/includes/git-aliases.ps1
@@ -2,15 +2,13 @@
 # GIT aliases missing in the plugin
 #------------------------------------------------------------------------------
 
-
 function _git_worktree {
     $cmd = switch($MyInvocation.InvocationName) {
-        gwt { $args }
         gwtls { 'list' }
         gwtmv { 'move' }
         gwtrm { 'remove' }
     }
-    git worktree $cmd
+    git worktree $cmd $args
 }
 Set-Alias -Value _git_worktree -Name gwt
 Set-Alias -Value _git_worktree -Name gwtls
