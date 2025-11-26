88666ae57f3e30f7a0d09c7c4103305d40703a30
OliverHR <113790+oliverhr@users.noreply.github.com>
2025-11-26 09:39:35 -0600

    Update zsh configuration

--- zsh/ohmy.zsh
+++ zsh/ohmy.zsh
@@ -67,7 +67,7 @@ ZSH_THEME="refined"
 # Custom plugins may be added to $ZSH_CUSTOM/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
-plugins=(git)
+plugins=(git tmux)
 
 source $ZSH/oh-my-zsh.sh
 
