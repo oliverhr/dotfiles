7f172b81eac7e01123aaf3dd870133f267cb9b04
OliverHR <113790+oliverhr@users.noreply.github.com>
2026-02-14 15:14:11 -0600

    Cleanup

--- linux/os.conf.zsh
+++
@@ -1,9 +0,0 @@
-# vim: set ft=zsh ts=4 sw=4 noet :
-# -----------------------------------------------------------------------------
-# Linux ZSH Settings
-# -----------------------------------------------------------------------------
-# export XDG_RUNTIME_DIR="/tmp/run/user/$(id -u $USER)"
-
-# homebrew
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-
