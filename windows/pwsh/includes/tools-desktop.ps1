07d9118f0de4c38326927df119a4528e746db9e5
OliverHR <113790+oliverhr@users.noreply.github.com>
2026-02-20 13:20:14 -0600

    Add function to open chrome with a MAP DNS

--- windows/pwsh/includes/tools-desktop.ps1
+++ windows/pwsh/includes/tools-desktop.ps1
@@ -19,4 +19,29 @@ Set-Alias -Name winmergeu -Value "$env:HOMEPATH\scoop\shims\winmergeu.exe"
 Set-Alias -Name winmerge -Value winmergeu
 Set-Alias -Name wm -Value winmergeu
 
+# Open Chrome based browser with a DNS Map
+function chromap {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$domain,
+
+        [Parameter(Mandatory = $false)]
+        [string]$url,
+
+        [Parameter(Mandatory = $false)]
+        [string]$browser = "$env:HOMEPATH\scoop\apps\ungoogled-chromium\current\chrome.exe"
+    )
+
+    $host_rule = '--host-rules="MAP {0} 127.0.0.1"' -f $domain
+    $args = @(
+        $host_rule
+         "--window-size=1440,880"
+         "--incognito"
+         "--auto-open-devtools-for-tabs"
+         $url
+    )
+
+    Start-Process -FilePath $browser -ArgumentList $args
+}
+
 # vim: set ft=ps1 ts=4 sts=4 sw=4 et :
