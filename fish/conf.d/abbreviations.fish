8867d9ef97114f5316b2b754e957158879d59d85
OliverHR <oliverhr@users.noreply.github.com>
2026-02-28 14:34:25 -0600

    Move some alias as abbreviations

---
+++ fish/conf.d/abbreviations.fish
@@ -0,0 +1,10 @@
+# ---------------------------------------------------------
+# Alias
+# ---------------------------------------------------------
+if status is-interactive
+
+    abbr --add dk 'docker'
+    abbr --add dkps 'docker container list'
+    abbr --add dki 'docker image list'
+
+end
--- fish/conf.d/alias.fish
+++ fish/conf.d/alias.fish
@@ -1,17 +1,13 @@
 # ---------------------------------------------------------
 # Alias
 # ---------------------------------------------------------
+if status is-interactive
 
-# alias ll 'ls -l'
+    alias vd 'vimdiff'
 
-alias vd 'vimdiff'
+    alias dkcl 'docker container ls --format "table {{.ID}}\t{{.Image}}\t{{.Names}}\t{{.Status}"'
+    alias dkil 'docker image ls --format "table {{.ID}}\t{{.Repository}}\t{{.Tag}}\t{{.CreatedAt}}\t{{.CreatedSince}}"'
 
-alias dk 'docker'
-alias dki 'docker images'
-alias dkps 'docker ps'
-alias dkcl 'docker container ls --format "table {{.ID}}\t{{.Image}}\t{{.Names}}\t{{.Status}"'
-alias dkil 'docker image ls --format "table {{.ID}}\t{{.Repository}}\t{{.Tag}}\t{{.CreatedAt}}\t{{.CreatedSince}}"'
+    alias typora 'open -a typora'
 
-alias typora 'open -a typora'
-
-
+end
