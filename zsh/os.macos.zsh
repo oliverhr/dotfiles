d3d283abe86eb207640900f258470d25ec70ddc0
OliverHR <oliverhr@users.noreply.github.com>
2026-03-03 05:31:35 -0600

    Add os zsh config

--- zsh/os.macos.zsh
+++ zsh/os.macos.zsh
@@ -1,4 +1,16 @@
 # vim: set ft=zsh ts=4 sw=4 noet :
 
+export PATH="/usr/local/sbin:$PATH"
+
 export CARAPACE_BRIDGES='zsh,fish,bash,inshellisense' # optional
 source <(carapace _carapace)
+
+# iTerm
+test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
+
+#RubyEnv
+eval "$(rbenv init - zsh)"
+
+# ---
+# Added by Antigravity
+export PATH="/Users/oliver/.antigravity/antigravity/bin:$PATH"
