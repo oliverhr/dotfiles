565038868e92cbae1420e7defefab720f329f071
OliverHR <113790+oliverhr@users.noreply.github.com>
2026-03-06 14:26:11 -0600

    Extract worktree clone function to a sh file

---
+++ git/shfn/treeclone.sh
@@ -0,0 +1,19 @@
+# vim: set ft=sh ts=4 sw=4 noet :
+
+if (( $# != 2 )); then
+	printf 'usage:\n\t git %s [repository url] [directory path]' "$GIT_ALIAS"
+	exit 0;
+fi
+
+_repo=${1}
+_path=${2}
+_wtd='.worktree.git'
+
+mkdir $_path && cd $_path
+
+git clone --bare $_repo $_wtd
+printf 'gitdir: %s' ${_wtd} > .git
+
+git config remote.origin.fetch '+refs/heads/*:refs/remotes/origin/*'
+git fetch origin --quiet
+
